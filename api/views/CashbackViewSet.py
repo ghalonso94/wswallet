b8be982cabf781c4d83b02bf8e372e0bda7ce27c
ghalonso94 <59257870+ghalonso94@users.noreply.github.com>
2021-07-30 01:21:10 -0300

    Implementados validações de tipo e soma dos itens

--- api/views/CashbackViewSet.py
+++ api/views/CashbackViewSet.py
@@ -1,5 +1,6 @@
 import json
 
+from django.db import transaction
 from django.http import HttpResponse
 from rest_framework import viewsets, status
 from rest_framework.authentication import BasicAuthentication
@@ -19,91 +20,116 @@ class CashbackViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
 
-        # Criando uma nova venda
-        sale = Sale()
-        sale.sold_at = request.data["sold_at"]
-        sale.company = Company.objects.filter(user=request.user).first()
+        with transaction.atomic():
 
-        # Tratando exceção para caso não envie o número da venda (não obrigatório)
-        try:
-            sale.number = request.data["number"]
-        except:
-            pass
+            # Criando uma nova venda
+            sale = Sale()
+            sale.sold_at = request.data["sold_at"]
+            sale.company = Company.objects.filter(user=request.user).first()
 
-        # Obtendo informações do consumidor
-        document = request.data["customer"]["document"]
-        name = request.data["customer"]["name"]
-
-        # Limpando dados do documento
-        document = str(document).replace('.','').replace('-','')
-
-        # Verificando se consumidor já existe
-        customer = Customer.objects.filter(document=document).first()
-
-        # Se não encontrar nenhum consumidor, cria um novo
-        if not customer:
-            # Validando CPF
-            cpf = CPF()
+            # Tratando exceção para caso não envie o número da venda (não obrigatório)
             try:
-                validation = cpf.validate(document)
-                if validation:
-                    customer = Customer(name=name, document=document)
-                    customer.save()
-                else:
+                sale.number = request.data["number"]
+            except:
+                pass
+
+            # Obtendo informações do consumidor
+            document = request.data["customer"]["document"]
+            name = request.data["customer"]["name"]
+
+            # Limpando dados do documento
+            document = str(document).replace('.','').replace('-','')
+
+            # Verificando se consumidor já existe
+            customer = Customer.objects.filter(document=document).first()
+
+            # Se não encontrar nenhum consumidor, cria um novo
+            if not customer:
+                # Validando CPF
+                cpf = CPF()
+                try:
+                    validation = cpf.validate(document)
+                    if validation:
+                        customer = Customer(name=name, document=document)
+                        customer.save()
+                    else:
+                        response = {
+                            'error': 'field "document" is invalid (CPF)'
+                        }
+                        return HttpResponse({json.dumps(response)}, status=status.HTTP_406_NOT_ACCEPTABLE, content_type='application/json')
+                except Exception as ex:
                     response = {
-                        'error': 'CPF inválido'
+                        'error': 'field "document" is invalid (CPF)'
                     }
                     return HttpResponse({json.dumps(response)}, status=status.HTTP_406_NOT_ACCEPTABLE, content_type='application/json')
-            except Exception as ex:
+
+            # Atribui o consumidor à venda e salva
+            sale.customer = customer
+            sale.save()
+
+            total = request.data["total"]
+
+            sale_item_list = []
+
+            total_conference = 0.00
+
+            validate = True
+            # Validando todos os tipos de produtos
+            for p in request.data["products"]:
+                if not p["type"] in ['A', 'B', 'C']:
+                    validate = False
+
+            if not validate:
+                sale.delete()
                 response = {
-                    'error': 'CPF inválido'
+                    'error': 'field "type" is invalid'
+                }
+                return HttpResponse({json.dumps(response)}, status=status.HTTP_406_NOT_ACCEPTABLE, content_type='application/json')
+            else:
+                # Obtendo lista de produtos e salvando amarrados à venda instanciada
+                for p in request.data["products"]:
+                    sale_item = SaleItem(sale=sale)
+                    sale_item.type = p["type"]
+                    sale_item.value = p["value"]
+                    sale_item.quantity = p["qty"]
+                    sale_item.save()
+                    total_conference += float(sale_item.value)
+                    sale_item_list.append(sale_item)
+
+            #Conferindo se os valores batem com o total
+            if float(total) != float(total_conference):
+                response = {
+                    'error': 'field "total" does not match the sum of the products'
                 }
                 return HttpResponse({json.dumps(response)}, status=status.HTTP_406_NOT_ACCEPTABLE, content_type='application/json')
 
-        # Atribui o consumidor à venda e salva
-        sale.customer = customer
-        sale.save()
+            # Montando dict dos itens para retornar na API
+            product_list = []
+            for sale_item in sale_item_list:
+                sale_item = {
+                    'product_id': str(sale_item.saleitem_id),
+                    'type': sale_item.type,
+                    'qty': str(sale_item.quantity),
+                    'value': str(sale_item.value)
+                }
+                product_list.append(sale_item)
 
-        total = request.data["total"]
-
-        sale_item_list = []
-
-        # Obtendo lista de produtos e salvando amarrados à venda instanciada
-        for p in request.data["products"]:
-            sale_item = SaleItem(sale=sale)
-            sale_item.type = p["type"]
-            sale_item.value = p["value"]
-            sale_item.quantity = p["qty"]
-            sale_item.save()
-            sale_item_list.append(sale_item)
-
-        # Montando dict dos itens para retornar na API
-        product_list = []
-        for sale_item in sale_item_list:
-            sale_item = {
-                'product_id': str(sale_item.saleitem_id),
-                'type': sale_item.type,
-                'qty': str(sale_item.quantity),
-                'value': str(sale_item.value)
+            # Montando dict do consumidor para retorno
+            customer = {
+                'customer_id': str(customer.customer_id),
+                'created_at': str(customer.created_at),
+                'document': customer.document,
+                'name': customer.name,
             }
-            product_list.append(sale_item)
 
-        # Montando dict do consumidor para retorno
-        customer = {
-            'customer_id': str(customer.customer_id),
-            'created_at': str(customer.created_at),
-            'document': customer.document,
-            'name': customer.name,
-        }
+            # Montando dict da venda total para retorno
+            sale = {
+                'sale_id': str(sale.sale_id),
+                'created_at': str(sale.created_at),
+                'sold_at': str(sale.sold_at),
+                'total': str(total),
+                'customer': customer,
+                'product_list': product_list
+            }
 
-        # Montando dict da venda total para retorno
-        sale = {
-            'sale_id': str(sale.sale_id),
-            'created_at': str(sale.created_at),
-            'sold_at': str(sale.sold_at),
-            'total': str(total),
-            'customer': customer,
-            'product_list': product_list
-        }
-
-        return HttpResponse({json.dumps(sale)}, status=status.HTTP_201_CREATED, content_type='application/json')
+            return HttpResponse({json.dumps(sale)}, status=status.HTTP_201_CREATED, content_type='application/json')
