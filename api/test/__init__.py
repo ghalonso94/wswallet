de7fb11b04d3956c6a57bd412291e40797d13daa
ghalonso94 <59257870+ghalonso94@users.noreply.github.com>
2021-08-01 12:18:17 -0300

    Teste unitário serializer Customer

--- api/test/__init__.py
+++ api/test/__init__.py
@@ -2,3 +2,4 @@ from api.test.test_serializers.company import CompanySerializerTestCase
 from api.test.test_serializers.cashback import CashbackSerializerTestCase
 from api.test.test_serializers.sale import SaleSerializerTestCase
 from api.test.test_serializers.saleitem import SaleItemSerializerTestCase
+from api.test.test_serializers.customer import CustomerSerializerTestCase
---
+++ api/test/test_serializers/customer.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from api.serializer import CompanySerializer, CustomerSerializer
+from core.models import Company, Customer
+
+
+class CustomerSerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.customer = Customer(
+            name='Company Test', document='00.000.000/0000-00'
+        )
+        self.serializer = CustomerSerializer(instance=self.customer)
+
+    def test_verify_fields_serialized(self):
+        """Verification test of Customer fields serializeds"""
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()),
+                         set(['created_at', 'updated_at', 'customer_id', 'name', 'document']))
+
+    def test_verify_content_fields_serialized(self):
+        """Verification test of Customer content fields serializeds"""
+        data = self.serializer.data
+        self.assertEqual(data['customer_id'], str(self.customer.customer_id))
+        self.assertEqual(data['name'], self.customer.name)
+        self.assertEqual(data['document'], self.customer.document)
+
