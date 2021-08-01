88de88802d54eaf525d01f067b06ce84ceb16c3c
ghalonso94 <59257870+ghalonso94@users.noreply.github.com>
2021-08-01 14:08:40 -0300

    Estruturando documentação com swagger

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,22 @@ from django.urls import path, include
 from . import views
 from .views import CompanyViewSet, CustomerViewSet, SaleViewSet, SaleItemViewSet, CashbackViewSet
 from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="WS Wallet",
+        default_version='v1',
+        description="API Rest to generate and register cashbacks for all partners companies of WS Wallet",
+        terms_of_service="#",
+        contact=openapi.Contact(email="contact@wswallet.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register('company', CompanyViewSet, basename='Company')
@@ -11,5 +27,6 @@ router.register('saleitem', SaleItemViewSet, basename='SaleItem')
 router.register('cashback', CashbackViewSet, basename='Cashback')
 
 urlpatterns = [
-    path('v1/', include(router.urls))
+    path('v1/', include(router.urls)),
+    path('v1/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-swagger-ui'),
 ]
